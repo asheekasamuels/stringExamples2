16f0a22354d78cb8f5c45e6129c12724bde8fef1
asheekasamuels <samuelsasheeka133@gmail.com>
2024-05-23 14:46:06 +0200

    math

--- script/code.js
+++ script/code.js
@@ -5,26 +5,77 @@
 // first character
 // console.log(`first character: ${sentence.charAt(0)}`);
 // last character
-// console.log(`last character: ${sentence[numOfChars- 1]}`);
+//  console.log(`last character: ${sentence[numOfChars- 1]}`);
 // check if the first character start with I vs i
-// console.log(`first character start with I? ${sentence.startsWith('I')}`);
+//  console.log(`first character start with I? ${sentence.startsWith('I')}`);
+// replace love with hate
+//  console.log(`replace love with hate: ${sentence.replace('love', 'hate')}`);
+//  console.log(`does it end with programming? ' ${sentence.endsWith('programming')}`);
+//  console.log(`includes: ${sentence.includes('love')}`);
+/*
+please work on the following methods;
+-split()
+-slice()
+-charAt()
+-concat()
+-indexOf()
+-lastIndexOf()
+-substring()
+-toUpperCase()
+-trim()
+-trimStart()
+-trimEnd()
+*/
 
-// greet function 
-function greet (name) {
-    console.log(`Hello ${name}`);
-}
-greet('Asheeka')
+// let sentence = "My name is Asheeka!";
+// let words = sentence.split(" ");
+// console.log(words);
 
-//multiply function 
-function multiply(a, z) {
-    console.log(`the result of ${a} multiplied by ${z} is ${a * z}`);
-}
-multiply(5, 5)
+// let str1 = "My LastName is Samuels";
+// let slicedStr = str1.slice(6);
+// console.log(slicedStr);
 
-//TableOfFive function
-function tableOfFive(num) {
-    let result = num * 7
-    console.log(`the result of ${num} multiplied by 7 is ${result}`)
-    return result
-    }
-tableOfFive(7)
+// let str2 = "Asheeka";
+// console.log(str2.charAt(1));
+
+// let str3 = "Asheeka";
+// let str4 = "samuels";
+// let greeting = str3.concat(", ", str4, "!");
+// console.log(greeting);
+
+// let str5 = "Zulkifl Shaik";
+// let index = str5.indexOf("world");
+// console.log(index);
+
+// let str6 = "My name is zulkifl zulkifl";
+// let lastIndex = str6.lastIndexOf("world");
+// console.log(lastIndex);
+
+// let str7 = "Hello world";
+// let subStr = str7.substring(6, 11);
+// console.log(subStr);
+
+// let str8 = "yellow";
+// let upperStr = str8.toUpperCase();
+// console.log(upperStr);
+
+// let str9 = "I hate you";
+// let trimmedStr = str9.trim();
+// console.log(trimmedStr);
+
+// let str10 = "Jamie Wyngaard";
+// let trimmedStartStr = str10.trimStart();
+// console.log(trimmedStartStr);
+
+// let str = "Toni Adams";
+// let trimmedEndStr = str.trimEnd();
+// console.log(trimmedEndStr);
+
+// console.log(math.random() * 10);
+
+/*
+please create an application that will let users submit their ID number using an input element and check to see if it is 13 characters long and it is a number:
+* Display the following data 
+- Day/month/year
+- Gender: Male or Female    
+*/
